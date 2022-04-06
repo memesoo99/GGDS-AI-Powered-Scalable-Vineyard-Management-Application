7223b50bdaaa3829d1339f610b6b0464b3c23c77
memesoo99 <genniferk1234@gmail.com>
2022-04-06 14:22:32 +0000

    add argparse

--- rfr.py
+++ rfr.py
@@ -7,13 +7,13 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 import numpy as np
 import os
-
+import argparse
 import pickle
 import matplotlib.pyplot as plot
 from feature_extraction import Contours
 from sklearn.model_selection import GridSearchCV
 
-def inference(regressor_path='regressor_model.pkl', csv_path='/workspace/hue_test.csv', image_path='/workspace/regression_data/images3', mask_path = '/workspace/regression_data/masks'):
+def inference(regressor_path, csv_path, image_path, mask_path):
     """
     model_path : str
         RFR model path
@@ -22,11 +22,12 @@ def inference(regressor_path='regressor_model.pkl', csv_path='/workspace/hue_tes
     """
     with open(regressor_path,"rb") as f:
         regressor = pickle.load(f)
-    # for i in os.listdir(image_path):
-    #     mask_name = i.split('.')[0]+'_masks.pkl'
-    #     print(f"{image_path}/{i}")
-    #     features = Contours(f"{mask_path}/{mask_name}", f"{image_path}/{i}",csv_path)
-    #     features.run()
+
+    for i in os.listdir(image_path):
+        mask_name = i.split('.')[0]+'_masks.pkl'
+        print(f"{image_path}/{i}")
+        features = Contours(f"{mask_path}/{mask_name}", f"{image_path}/{i}",csv_path)
+        features.run()
 
     df = pd.read_csv(csv_path)
     X = df.drop(["image"], axis=1).values
@@ -36,17 +37,13 @@ def inference(regressor_path='regressor_model.pkl', csv_path='/workspace/hue_tes
     pred_df = pd.DataFrame(pred)
     n = len(df.columns)
     df.insert(n,'predict',pred_df)
-    print(df.head())
-    print(type(pred))
     result_df = pd.DataFrame({'Image':df["image"].to_numpy().reshape(-1), 'Predicted Values':pred.reshape(-1)})
-    print(result_df)
+    # print(result_df)
     df.to_csv(csv_path)
-
-def check_accuracy():
-    pass
 
 #train
 def train(regressor_path = "regressor_model.pkl",csv_path = '/workspace/features3.csv'):
+
     # for i in os.listdir('/workspace/regression_data/images3'):
     #     print(i)
     #     # image_name = i.replace('.pkl','.jpg')
@@ -79,22 +76,11 @@ def train(regressor_path = "regressor_model.pkl",csv_path = '/workspace/features
         if image_name not in gt_dict.keys():
             continue
         i.append(gt_dict[image_name])
-
-        #아래 부분 aspect ratio 정상 출력한 다음에 제거해야됨
-        # if math.isnan(i[-2]):
-       
-        #     i[-2] = i[-4]
-        # else: 
-        #     i[-4] = i[-2]
-
-        # i = i[:-3]+i[-2:]
-
         gt_df.loc[len(gt_df)] = i
 
     gt_df.to_csv('train_features.csv',index = False)
     X = gt_df.drop(["gt", "image"], axis=1).values
     Y = gt_df["gt"].values
-
     X_train, X_test, y_train, y_test = train_test_split(X,Y , test_size= 0.1)
 
     # Grid Search
@@ -102,20 +88,16 @@ def train(regressor_path = "regressor_model.pkl",csv_path = '/workspace/features
     regressor = RandomForestRegressor(random_state = 0, n_jobs = -1)
     grid_cv = GridSearchCV(regressor, param_grid = params, cv=5, n_jobs=-1)
     grid_cv.fit(X_train, y_train)
-
     best_param = grid_cv.best_params_
 
     print('최적 하이퍼 파라미터:', best_param)
-    # print('최고 예측 정확도: {:.4f}'.format(grid_cv.best_score_))
     estimator = grid_cv.best_estimator_
-
     regressor = RandomForestRegressor(random_state = 0, n_jobs = -1, criterion ='mse',max_depth = best_param['max_depth'],min_samples_leaf = best_param['min_samples_leaf'],n_estimators = best_param['n_estimators'],min_samples_split = best_param['min_samples_split'])
     regressor.fit(X_train,y_train)
 
     from sklearn.metrics import mean_squared_error
     some_predicted = regressor.predict(X_test)
     mse = np.sqrt(mean_squared_error(some_predicted, y_test))
-
     print('평균제곱근오차', mse)
 
     with open(regressor_path,"wb") as f:
@@ -125,8 +107,24 @@ def train(regressor_path = "regressor_model.pkl",csv_path = '/workspace/features
     result_df = pd.DataFrame({'Real Values':y_test.reshape(-1), 'Predicted Values':y_pred.reshape(-1)})
     print(result_df)
 
-    # print(f'테스트 데이터 세트 정확도: {0:.4f}'.format(accuracy_score(y_test,y_pred)))
-
-inference()
-
-# train()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Select inference/save mode')
+    parser.add_argument('--train', action='store_true', default=False,
+                        help='an integer for the accumulator')
+    parser.add_argument('--inference',action='store_true', default=False,
+                        help='sum the integers (default: find the max)')
+    parser.add_argument('--regressor-path', type=str, default='regressor_model.pkl',
+                        help='Trained regressor model path')
+    parser.add_argument('--csv-path', type=str, default='/workspace/hue_test.csv',
+                        help='csv file path where you want to save results')
+    parser.add_argument('--image-path', type=str, default='/workspace/regression_data/images3',
+                        help='inference할 cropped된 포도 이미지 folder')
+    parser.add_argument('--mask-path', type=str, default='/workspace/regression_data/masks',
+                        help='inference할 cropped된 포도 이미지 folder')
+    
+    args = parser.parse_args()
+    if inference:
+        inference(regressor_path = args.regressor_path, csv_path=args.csv_path, image_path= args.image_path, mask_path=args.mask_path)
+    if train:
+        train()
+    # train()
